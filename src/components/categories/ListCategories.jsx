cf0e88e9df7a631814e71d0923c3c7f9c06cd0de
hafedhdhouib <hafedhdhouib@gmail.com>
2024-12-16 20:25:06 +0100

    categories done

--- src/components/categories/ListCategories.jsx
+++ src/components/categories/ListCategories.jsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import ViewCategories from "./ViewCategories";
 const Listcategories = () => {
   const [categories, setCategories] = useState([]);
   const getcategories = async () => {
@@ -17,34 +20,20 @@ const Listcategories = () => {
   }, []);
   return (
     <div>
+      <Button variant="contained" style={{ backgroundColor: "black" }}>
+        <Link
+          to="/categories/add"
+          style={{
+            color: "white",
+
+            textDecoration: "none",
+          }}
+        >
+          <i className="fa-solid fa-plus-square"></i> Nouveau
+        </Link>
+      </Button>
       <h2>Liste des catégories </h2>
-      <table className="table table table-striped">
-        <thead>
-          <tr>
-            <td>Nom catégorie</td>
-            <td>Image categorie</td>
-            <td>Update</td>
-            <td>Delete</td>
-          </tr>
-        </thead>
-        <tbody>
-          {categories &&
-            categories.map((cat, index) => (
-              <tr key={index}>
-                <td>{cat.nomcategorie}</td>
-                <td>
-                  <img src={cat.imagecategorie} width={100} height={100} />
-                </td>
-                <td>
-                  <button className="btn btn-warning btn-sm">Update</button>
-                </td>
-                <td>
-                  <button className="btn btn-danger btn-sm">Delete</button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <ViewCategories categories={categories} setCategories={setCategories} />
     </div>
   );
 };
